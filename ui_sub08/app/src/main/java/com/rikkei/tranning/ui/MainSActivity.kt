a1d4d15e12303f988597c75059d7aa5c5eefe82b
DucTrungNguyen <ductrungnguyen98@gmail.com>
2020-07-18 13:20:06 +0700

    subcoe08

---
+++ ui_sub08/app/src/main/java/com/rikkei/tranning/ui/MainSActivity.kt
@@ -0,0 +1,11 @@
+package com.rikkei.tranning.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainSActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_s)
+    }
+}
